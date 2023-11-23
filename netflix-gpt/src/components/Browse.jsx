f6a5e0e8bfbdc401d3f59ce8a16a16b291df98bb
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-11-23 13:43:28 +0530

    done with MainCOntainer

--- netflix-gpt/src/components/Browse.jsx
+++ netflix-gpt/src/components/Browse.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Header from "./Header";
+import useNowPlayingMovies from "../utils/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from './SecondaryContainer'
 
 const Browse = () => {
+
+  useNowPlayingMovies();
+
   return (
     <div>
-      <Header/>
-      
+      <Header />
+      {/* Design Of Browse Page
+        - Main Container
+          - VideoBg
+          - VideoTitle
+        - Secondary Container
+          - Movielist * n
+          - Cards * n 
+      */}
+      <MainContainer/>
+      <SecondaryContainer/>
     </div>
-  )
-}
+  );
+};
 
-export default Browse
+export default Browse;
